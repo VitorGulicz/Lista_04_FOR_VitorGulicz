56ca00f63ef8fd12e9eab1248bddc0a41f56ca35
VitorGulicz <vdmgulicz@gmail.com>
2025-02-17 22:08:58 -0300

    update

--- Exe06_VitorGulicz.py
+++ Exe06_VitorGulicz.py
@@ -1,5 +1,5 @@
 #Peça um numero abaixo de 50 e em seguida faça uma contagem regressiva de 50 até esse numero, certificando-se de mostrar o numero que eles inseriram na saída
-numero=int(input("Digite um numero abaixo de 50"))
+numero=int(input("Digite um numero abaixo de 50: "))
 if numero<=50:
     i=50
     numeros=numero
